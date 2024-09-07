abc085425c48c46a43428310f2a29725596fdd8f
krishna sharma <ks.creative2408@gmail.com>
2024-09-08 00:51:59 +0530

    Added table 'Guests' using Models

--- hotel_booking_system/booking/models.py
+++ hotel_booking_system/booking/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Guests(models.Model):
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    check_in_dates = models.DateField()
+    check_out_dates = models.DateField()
+    room_number = models.IntegerField(max_length=3)
+    type_of_room = models.CharField(max_length=10)
+    package_included = models.BooleanField()
+
+def __str__(self):
+    return f"{self.first_name} , {self.last_name} , {self.type_of_room}"
